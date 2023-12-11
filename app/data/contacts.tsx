efb8a28f971155f535dcc68b91fc6e483504d250
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2023-12-11 12:30:30 +0100

    add test data

---
+++ app/data/contacts.tsx
@@ -0,0 +1,319 @@
+const contacts =[
+    {
+        "dn": "cn=09097749369,ou=contacts,o=fint",
+        "nin": "12345678901",
+        "firstName": "Per Arne",
+        "lastName": "Olsen",
+        "mail": "tor.endre.v.bakken@agderfk.no",
+        "mobile": "98895845",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=11223344556,ou=contacts,o=fint",
+        "nin": "23456789012",
+        "firstName": "Anna",
+        "lastName": "Larsen",
+        "mail": "anna.larsen@example.com",
+        "mobile": "12345678",
+        "technical": [
+            "ou=vlfk_no,ou=organisations,o=fint"
+        ],
+        "legal": [],
+        "supportId": "9834077889308",
+        "roles": []
+    },
+    {
+        "dn": "cn=22334455667,ou=contacts,o=fint",
+        "nin": "34567890123",
+        "firstName": "Erik",
+        "lastName": "Olsen",
+        "mail": "erik.olsen@example.com",
+        "mobile": "98765432",
+        "technical": [
+            "ou=example_org2,ou=technical,o=fint"
+        ],
+        "legal": [],
+        "supportId": "7890123456789",
+        "roles": []
+    },
+    {
+        "dn": "cn=33445566778,ou=contacts,o=fint",
+        "nin": "45678901234",
+        "firstName": "Linda",
+        "lastName": "Andersen",
+        "mail": "linda.andersen@example.com",
+        "mobile": "87654321",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=44556677889,ou=contacts,o=fint",
+        "nin": "56789012345",
+        "firstName": "Mark",
+        "lastName": "Peterson",
+        "mail": "mark.peterson@example.com",
+        "mobile": "13579246",
+        "technical": [
+            "ou=viken_no,ou=organisations,o=fint"
+        ],
+        "legal": [],
+        "supportId": "10629331764124",
+        "roles": []
+    },
+    {
+        "dn": "cn=55667788990,ou=contacts,o=fint",
+        "nin": "67890123456",
+        "firstName": "Sara",
+        "lastName": "Jensen",
+        "mail": "sara.jensen@example.com",
+        "mobile": "24681357",
+        "technical": [
+            "ou=rogfk_no,ou=organisations,o=fint"
+        ],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=66778899001,ou=contacts,o=fint",
+        "nin": "78901234567",
+        "firstName": "John",
+        "lastName": "Doe",
+        "mail": "john.doe@example.com",
+        "mobile": "98765432",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=77889900112,ou=contacts,o=fint",
+        "nin": "89012345678",
+        "firstName": "Alice",
+        "lastName": "Johnson",
+        "mail": "alice.johnson@example.com",
+        "mobile": "87654321",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=88990011223,ou=contacts,o=fint",
+        "nin": "90123456789",
+        "firstName": "Chris",
+        "lastName": "Williams",
+        "mail": "chris.williams@example.com",
+        "mobile": "98761234",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=99001112234,ou=contacts,o=fint",
+        "nin": "01234567890",
+        "firstName": "Emma",
+        "lastName": "Brown",
+        "mail": "emma.brown@example.com",
+        "mobile": "54321678",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=00112233445,ou=contacts,o=fint",
+        "nin": "98765432101",
+        "firstName": "David",
+        "lastName": "Lee",
+        "mail": "david.lee@example.com",
+        "mobile": "10293847",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=11223344556,ou=contacts,o=fint",
+        "nin": "87654321012",
+        "firstName": "Sophie",
+        "lastName": "Smith",
+        "mail": "sophie.smith@example.com",
+        "mobile": "56789012",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=22334455667,ou=contacts,o=fint",
+        "nin": "76543210923",
+        "firstName": "Alex",
+        "lastName": "Taylor",
+        "mail": "alex.taylor@example.com",
+        "mobile": "43210987",
+        "technical": [
+            "ou=rogfk_no,ou=organisations,o=fint",
+            "ou=oslo_kommune_no,ou=organisations,o=fint"
+        ],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=33445566778,ou=contacts,o=fint",
+        "nin": "65432109876",
+        "firstName": "Julia",
+        "lastName": "Miller",
+        "mail": "julia.miller@example.com",
+        "mobile": "98765432",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=44556677889,ou=contacts,o=fint",
+        "nin": "54321098765",
+        "firstName": "Tom",
+        "lastName": "Anderson",
+        "mail": "tom.anderson@example.com",
+        "mobile": "87654321",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=55667788990,ou=contacts,o=fint",
+        "nin": "43210987654",
+        "firstName": "Mia",
+        "lastName": "Johnson",
+        "mail": "mia.johnson@example.com",
+        "mobile": "98765432",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=66778899001,ou=contacts,o=fint",
+        "nin": "32109876543",
+        "firstName": "Peter",
+        "lastName": "White",
+        "mail": "peter.white@example.com",
+        "mobile": "87654321",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=77889900112,ou=contacts,o=fint",
+        "nin": "21098765432",
+        "firstName": "Olivia",
+        "lastName": "Clark",
+        "mail": "olivia.clark@example.com",
+        "mobile": "76543210",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=88990011223,ou=contacts,o=fint",
+        "nin": "10987654321",
+        "firstName": "William",
+        "lastName": "Baker",
+        "mail": "william.baker@example.com",
+        "mobile": "54321098",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=99001112234,ou=contacts,o=fint",
+        "nin": "98765432109",
+        "firstName": "Sophia",
+        "lastName": "Hill",
+        "mail": "sophia.hill@example.com",
+        "mobile": "32109876",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=00112233445,ou=contacts,o=fint",
+        "nin": "87654321098",
+        "firstName": "Daniel",
+        "lastName": "Young",
+        "mail": "daniel.young@example.com",
+        "mobile": "98765432",
+        "technical": [
+            "ou=tromsfylke_no,ou=organisations,o=fint"
+        ],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=11223344556,ou=contacts,o=fint",
+        "nin": "76543210987",
+        "firstName": "Emily",
+        "lastName": "Martin",
+        "mail": "emily.martin@example.com",
+        "mobile": "54321098",
+        "technical": [
+            "ou=fridiks_no,ou=organisations,o=fint"
+        ],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=22334455667,ou=contacts,o=fint",
+        "nin": "65432109876",
+        "firstName": "Christopher",
+        "lastName": "Ward",
+        "mail": "christopher.ward@example.com",
+        "mobile": "98765432",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=33445566778,ou=contacts,o=fint",
+        "nin": "54321098765",
+        "firstName": "Ava",
+        "lastName": "Carter",
+        "mail": "ava.carter@example.com",
+        "mobile": "87654321",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    },
+    {
+        "dn": "cn=44556677889,ou=contacts,o=fint",
+        "nin": "43210987654",
+        "firstName": "James",
+        "lastName": "Perry",
+        "mail": "james.perry@example.com",
+        "mobile": "98765432",
+        "technical": [],
+        "legal": [],
+        "supportId": null,
+        "roles": []
+    }
+]
+
+export default contacts;
