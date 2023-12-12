dab930ccb08ec4bdafe2171c3fe1aa71d4dee764
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2023-12-12 09:08:38 +0100

    add search on component

--- app/routes/component._index.tsx
+++ app/routes/component._index.tsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styled from 'styled-components';
 import { InternalHeader, Spacer, Search } from "@navikt/ds-react";
 import {ComponentIcon, PersonPlusIcon} from "@navikt/aksel-icons";
@@ -6,6 +6,7 @@ import ComponentsTable from "~/components/components-table";
 import components from "~/data/components";
 import CustomFormModal from "~/components/contact-add";
 import ComponentAdd from "~/components/component-add";
+import {IComponent} from "~/data/types";
 
 // Styled InternalHeader
 const StyledInternalHeader = styled(InternalHeader)`
@@ -16,27 +17,32 @@ const StyledInternalHeader = styled(InternalHeader)`
   --ac-internalheader-active-bg: var(--a-surface-inverted-active);
 
 `;
-// Styled Search
-const StyledSearch = styled(Search)`
-  input {
-    --ac-textfield-bg: var(--a-border-subtle);
-    --ac-textfield-border: var(--a-border-default);
-    --ac-textfield-text: var(--a-text-default);
-    --ac-textfield-placeholder: var(--a-text-subtle);
-    --ac-textfield-hover-border: var(--a-border-action);
-    --ac-textfield-error-border: var(--a-border-danger);
-
-  }
-`;
 
 const ComponentPage = () => {
     const componentEditRef = useRef<HTMLDialogElement>(null);
+    const [searchInput, setSearchInput] = useState("");
+    const [filteredData, setFilteredData] = useState<IComponent[]>([])
+
+    useEffect(() => {
+        setFilteredData(components)
+    }, []);
+
 
     const handleFormClose = () => {
         // Handle form submission logic
         console.log("closing the contact add form inside index");
         componentEditRef?.current?.close();
     };
+
+    const handleSearchInput = (input:any) => {
+        setSearchInput(input);
+        const filtered = components.filter(
+            (row) =>
+                row.name.toLowerCase().includes(input.toLowerCase()) ||
+                row.description.toLowerCase().includes(input.toLowerCase())
+        );
+        setFilteredData(filtered)
+    }
 
     return (
         <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
@@ -61,16 +67,17 @@ const ComponentPage = () => {
                         console.log("Search!");
                     }}
                 >
-                    <StyledSearch
+                    <Search
                         label="InternalHeader søk"
                         size="medium"
                         variant="simple"
                         placeholder="Søk"
+                        onChange={handleSearchInput}
                     />
                 </form>
             </StyledInternalHeader>
 
-            <ComponentsTable data={components} />
+            <ComponentsTable data={filteredData} />
         </div>
     );
 };
