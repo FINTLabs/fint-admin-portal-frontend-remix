3426464244e4605c066881bb4d4706d59276d72a
jennifer-fintlabs <jennifer@fintlabs.no>
2024-01-11 16:11:42 +0100

    tab edit, breadcrumbs formatting, other small fixes

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,9 @@
+// cypress/integration/example_spec.js
+
+describe('My First Test', () => {
+  it('Visits the app', () => {
+    cy.visit('http://localhost:3000'); // Replace with your actual app URL
+    cy.contains('Novari Logo Here'); // Replace with your actual text/content
+  });
+});
+
