63e4fe13756409aea8e241de88755a0a0b7edfab
jennifer-fintlabs <jennifer@fintlabs.no>
2024-07-03 13:02:45 +0200

    added full header 🐶

--- app/api/me-api.tsx
+++ app/api/me-api.tsx
@@ -5,7 +5,7 @@ const API_URL = process.env.API_URL || 'http://fint-admin-portal-backend:8080';
 class MeApi {
     static async fetchDisplayName(headers: HeadersInit | undefined) {
         log("Fetching display name", API_URL);
-        // log("COOKIES for me", cookies);
+        log("COOKIES for me", headers);
         try {
             const response = await fetch(`${API_URL}/api/me`, {
                 method: 'GET',
